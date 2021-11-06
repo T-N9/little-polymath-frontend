311ac8e89ebe7ef714c47d0434ce6009693d69c4
T-N9 <tenyainmoelwin@gmail.com>
2021-11-06 20:09:44 +0630

    migrate to arch

--- lp-root/assets/js/lp-quiz.js
+++ lp-root/assets/js/lp-quiz.js
@@ -35,7 +35,7 @@ let userAns = null; /* temp var to check condition user selected answer */
 /* ---------------------------------------------------- */
 // creating template for each quiz
 /* ---------------------------------------------------- */
-let quiz_i = 0;
+let quiz_i = 4;
 const lpQuesNo = document.querySelector('.lp-no')
 const lpCategory = document.querySelector('.lp-category');
 const lpQuestion = document.querySelector('.lp-question');
@@ -60,6 +60,7 @@ const displayQuiz = (i) => {
         
         if (time_i <= 0) {
             clearInterval(timer);
+            questionResult();
         }
 
         if (time_i <= 13){
@@ -101,15 +102,75 @@ const nextQuestion = () => {
 /* ---------------------------------------------------- */
 // validate user answer with answer in quizSetData
 /* ---------------------------------------------------- */
-let score_Timer;
+let clickedBtnIndex; /* Clicked Button Index */
+let ansIndex;
+const optionButtons = Array.from(document.getElementsByClassName('op-button'));
+optionButtons.forEach(btn => {
+    btn.addEventListener('dblclick', ()=>{
+        optionButtons.forEach(btnClass => {
+            btnClass.classList.value = "op-button";
+        })
+        btn.classList.add("selected");
+        clickedBtnIndex = parseInt(btn.getAttribute('ondblclick').slice(12,-1));
+        return clickedBtnIndex;
+    });
+});
+
+let timer_Score; /* Get Time of user answer */
+let score_Timer; /* Store time of user answer */
 const validateAns = (ans) => {
-    score_Timer = parseInt(document.querySelector('.score-timer').textContent);
+    timer_Score = document.querySelector('.score-timer');
+    score_Timer = parseInt(timer_Score.textContent);
+
+    for(let a=0 ; a <4; a++){
+        if(quizSetData[quiz_i].option[a] === quizSetData[quiz_i].answer) {
+            ansIndex = a;
+        }
+    }
+
     if(quizSetData[quiz_i].option[ans] === quizSetData[quiz_i].answer){
         userAns = true;
         console.log("TRUE",quizSetData[quiz_i].option[ans]);
+        console.log("TRUE at "+ ans);
     }else {
         userAns = false;
         console.log("FALSE",quizSetData[quiz_i].option[ans]);
     }
-    return userAns, score_Timer;
+    console.log(score_Timer);
+    return userAns, score_Timer, ansIndex;
+}
+
+/* ---------------------------------------------------- */
+// result for each question
+/* ---------------------------------------------------- */
+let score = 0;
+let life = 3;
+
+const questionResult = () => {
+    let resultText; 
+    if(userAns === true){
+        resultText = "သင့်အဖြေ မှန်ကန်ပါသည်။";
+        score = score + 10 + score_Timer;
+    }else if (userAns === false){
+        resultText = "သင့်အဖြေ မှားယွင်းနေပါသည်။";
+        life = life -1;
+    }else {
+        resultText = "သင် ဖြေဆိုထားခြင်းမရှိပါ။";
+        life = life -1;
+    }
+    if(life > 0) {
+    //     document.querySelector('.result').innerHTML = `
+    //     <h2>အမှတ် ${en2mm(score)}</h2>
+    //     <h3>${resultText}</h3>
+    //     <p>${quizSetData[quiz_i].answer}</p>
+    //     <small>${quizSetData[quiz_i].explanation}</small>
+    //     <button onclick="nextQuestion()">Next</button>
+    // `;
+
+    console.log(clickedBtnIndex, ansIndex)
+    } else {
+        document.querySelector('.result').innerHTML = `
+        <h1>You Lose !!!</h1>
+    `;
+    }
 }
